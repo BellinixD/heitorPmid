bcff69528b02f8ded1df105f69fce28a8bc82875
FelipeAlencarGit <76817229+FelipeAlencarGit@users.noreply.github.com>
2020-12-31 12:20:18 -0300

    Delete devLogin.sh

--- cmd/shell/devLogin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcloud config set project tim-pmid-dev
-gcloud config set compute/region southamerica-east1
-gcloud container clusters get-credentials pmid-dev --region=southamerica-east1
