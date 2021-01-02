736e4302679a994cc3f9af292bf46be6a631f334
FelipeAlencarGit <76817229+FelipeAlencarGit@users.noreply.github.com>
2021-01-02 00:28:32 -0300

    Add files via upload

--- cmd/shell/execPod.sh
+++ cmd/shell/execPod.sh
@@ -6,6 +6,6 @@
 #execPod fornece acesso de root ao pod informado.
 #
 #Como usar:
-#. execPod.sh u-billing-profile-info-v1-6cf54c8584-kzrmg
+#. execPod.sh u-billing-profile-info-v1
 read -r np<namespace.txt
 kubectl -n $np exec -it deploy/$1 /bin/bash
