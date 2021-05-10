85568fb88e97d0fa65d837e98e21a386211d2526
BellinixD <55899476+BellinixD@users.noreply.github.com>
2021-05-10 11:32:10 -0300

    Atualização de cabeçalho

--- changeNamespace.sh
+++ changeNamespace.sh
@@ -1,10 +1,17 @@
-	#!/bin/bash
-#Histórico de versões:
+  
+#!/bin/bash
 #
-#1.0 - Heitor Bellini
+# changeNamespace - Muda a namespace atual.
 #
-#changeNamespace altera a namespace atual.
+# Como usar: ". changeNamespace.sh [NAMESPACE]"
 #
-#Como usar:
-#. changeNamespace.sh uat2
+# Exemplo:
+#             . changeNamespace.sh uat2
+#
+# Histórico de Versão:
+#       Versão: 1.0
+#             Autor: Heitor Bellini <heitor.bellini@engdb.com.br>
+#             Data: 01/2021
+#             Descrição: Primeira versão.
+
 sed -i "1 s/^.*$/$1/" namespace.txt
