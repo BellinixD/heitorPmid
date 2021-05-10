2183692235d2bf912ee02c3454be96c9bfdce428
BellinixD <55899476+BellinixD@users.noreply.github.com>
2021-05-10 12:00:53 -0300

    Atualização de cabeçalho

--- lessPod.sh
+++ lessPod.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#Histórico de versões:
 #
-#1.1 - Flavio Moreira
-#condição para o ambiente ser variável (caso não informe o ambiente será considerado o do namespace.txt)
+# lessPod.sh - abre o log atual do pod informado em formato 'less'.
 #
-#1.0 - Heitor Bellini
-#lessPod abre o log atual do pod informado em formato 'less'.
+# Como usar: ". getPods.sh nome-do-pod"
 #
-#Como usar:
-# . lessPod.sh imdb-read-status-by-msisdn-v1-7f4db868c5-pbhmr
-# . lessPod.sh imdb-read-status-by-msisdn-v1-7f4db868c5-pbhmr uat3
+# Exemplo:
+#             . lessPod.sh imdb-read-status-by-msisdn-v1-7f4db868c5-pbhmr
+#             . lessPod.sh imdb-read-status-by-msisdn-v1-7f4db868c5-pbhmr uat3
 #
+# Histórico de versões:
+#       Versão: 1.0
+#             Autor: Heitor Bellini <heitor.bellini@engdb.com.br>
+#             Data: 06/2020
+#             Descrição: Primeira versão.
+#       Versão: 1.1
+#             Autor: Flavio Moreira <flavio.moreira@engdb.com.br>
+#             Data: 04/2021
+#             Descrição: condição para o ambiente ser variável (caso não informe o ambiente será considerado o do namespace.txt)
+
 if [ $2 null ]
 then
     read -r np<namespace.txt
