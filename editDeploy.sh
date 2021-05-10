a269c9a9304d034260a27c9a98e900eb65c0f5c3
BellinixD <55899476+BellinixD@users.noreply.github.com>
2021-05-10 11:52:20 -0300

    Atualização de cabeçalho

--- editDeploy.sh
+++ editDeploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#Histórico de versões:
 #
-#1.1 - Flavio Moreira
-#condição para o ambiente ser variável (caso não informe o ambiente será considerado o do namespace.txt)
+# editDeploy.sh - possibilita editar o deployment de um serviço.
 #
-#1.0 - Felipe de Carvalho Alencar
+# Como usar: ". editDeploy.sh nome-do-pod-v1"
 #
-#editPod possibilita editar o deployment de um serviço
+# Exemplo:
+#             . editDeploy.sh u-billing-profile-info-v1
+#             . editDeploy.sh u-billing-profile-info-v1 uat2
 #
-#Como usar:
-#. editDeploy.sh u-billing-profile-info-v1
-#
+# Histórico de versões:
+#       Versão: 1.0
+#             Autor: Felipe de Carvalho Alencar <felipe.alencar@engdb.com.br>
+#             Data: 12/2020
+#             Descrição: Primeira versão.
+#       Versão: 1.1
+#             Autor: Flavio Moreira <flavio.moreira@engdb.com.br>
+#             Data: 04/2021
+#             Descrição: condição para o ambiente ser variável (caso não informe o ambiente será considerado o do namespace.txt)
+
 if [ $2 null ]
 then
     read -r np<namespace.txt
